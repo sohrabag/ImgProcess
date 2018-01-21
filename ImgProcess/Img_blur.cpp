557ef2df6db0ab54ff2811c5c2229f021472089f
sohrabag <33716625+sohrabag@users.noreply.github.com>
2018-01-22 00:15:24 +0100

    Added code to main program: reading of input, flipping of kernel matrix and convolution of that with image matrix

--- ImgProcess/Img_blur.cpp
+++ ImgProcess/Img_blur.cpp
@@ -6,12 +6,16 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stack>
 
 int main(void) {
 
 	//Img vector
 	std::vector< std::vector<int> > mat_Img;
 	std::vector < std::vector<int> > mat_kernel;
+	std::vector < std::vector<int> > mat_out_Img;
+	std::stack <int> flip_stk;
+	int pop_elm(0);
 	std::vector<int> H_W_N_M_list;
 
 	//reading input data
@@ -50,7 +54,7 @@ int main(void) {
 	for (int i = 0; i < H; ++i) {
 			std::getline(std::cin, strline);
 			if(i > 0) mat_Img[i].resize(W);
-#if 1
+
 			start = 0; found = 0;
 			found = strline.find_first_of(' ');
 			if (found != std::string::npos) {
@@ -59,7 +63,7 @@ int main(void) {
 			}
 			else
 				return 1;
-#endif
+
 			for (int j = 1; j < W; ++j) {
 
 				found = strline.find_first_of(' ', found + 1);
@@ -71,6 +75,67 @@ int main(void) {
 			mat_Img[i][W-1] = ::atoi(strline.substr(start + 1, strline.length() - start + 1).c_str());
 		}
 
+	//Extract Kernel values from input
+	start = found = 0; strline.clear();
+	mat_kernel.resize(N); //M is the width of the kernel matrix
+	mat_kernel[0].resize(M); // N is the height of the kernel matrix
+
+	for (int i = 0; i < N; ++i) {
+		if (i > 0) mat_kernel[i].resize(M);
+		std::getline(std::cin, strline);
+
+		start = found = 0;
+		found = strline.find_first_of(' ');
+		if (found != std::string::npos)
+			mat_kernel[i][0] = ::atoi(strline.substr(start, found - start).c_str());
+		else
+			return 1;
+
+		start = found;
+
+		for (int j = 1; j < M; j++) {
+
+			found = strline.find_first_of(' ', found + 1);
+			if (found != std::string::npos)
+				mat_kernel[i][j] = ::atoi(strline.substr(start, found - start).c_str());
+			else
+				mat_kernel[i][j] = ::atoi(strline.substr(start+1, strline.length() - start+1).c_str());
+
+			start = found;
+		}
+	}
+
+	//flip row and columns of the kernel matrix
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < M; ++j)
+			flip_stk.push(mat_kernel[i][j]);
+
+	//pop the values out of stack and put it inside the new kernel matrix
+	for(int i = 0; i < N; ++i)
+		for (int j = 0; j < M; ++j) {
+			pop_elm = flip_stk.top();
+			flip_stk.pop();
+			mat_kernel[i][j] = pop_elm;
+		}
+
+	//linear filtering using convolution
+	mat_out_Img.resize(H - 1);
+	for (int i = 0; i < H - 1; ++i)
+		mat_out_Img[i].resize(W - 1);
+
+	int res(0);
+	for (int i = 0; i < H - 1; ++i)
+		for (int j = 0; j < W - 1; ++j) {
+			res = 0;
+			for (int k = 0; k < N; ++k) {
+				for (int l = 0; l < M; ++l)
+				{
+					res += mat_Img[i + k][j + l] * mat_kernel[k][l];
+				}
+			}
+			std::cout << res << " ";
+			mat_out_Img[i][j] = res;
+		}
 
 	return 0;
 }
